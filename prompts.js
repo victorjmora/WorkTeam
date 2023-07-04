99e078be1bfe4b645b04eb13f6b65fbfa32febfc
victormora1 <victorjmora1@gmail.com>
2023-07-04 01:03:24 -0400

    fixed bugs in prompt.js that werent allowing me to test the application

--- prompts.js
+++ prompts.js
@@ -31,6 +31,7 @@ function start() {
         case "View all departments":
           viewAllDepartments();
           break;
+
         case "Add a department":
           addDepartment();
           break;
@@ -203,7 +204,7 @@ function updateEmployeeRole() {
   inquirer
     .prompt([
       {
-        name: "id",
+        name: "employee_id",
         type: "input",
         message: "what is the employee's id?",
       },
@@ -216,7 +217,7 @@ function updateEmployeeRole() {
     .then((answers) => {
       const { id, role_id } = answers;
       const query = `UPDATE employee SET role_id = ? WHERE ID = ?`;
-      connection.query(query, [id, role_id], (err, res) => {
+      connection.query(query, [role_id, id], (err, res) => {
         if (err) throw err;
         console.log("Employee role updated successfully");
         start();
